3ef3b2c9aa5ec816415b50f94247599c66ace872
Sumit Patel <76784329+sumit2401@users.noreply.github.com>
2022-11-18 11:59:44 +0530

    Update insertionsort.java

--- insertionsort.java
+++ insertionsort.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 public class insertionsort {
     public static void main(String[] args) {
         int[] arr={4,5,1,2,3};
-        insertion(arr);
+        inverseinsertion(arr);
         System.out.println(Arrays.toString(arr));
     }
-    static void insertion(int[] arr){
+    static void insertion(int[] arr){                 //output{1,2,3,4,5}
         for(int i =0; i<arr.length-1;i++){
         for(int j = i+1; j>0; j--){
                 if(arr[j]<arr[j-1]){
@@ -18,6 +18,18 @@ public class insertionsort {
             }
         }
 
+    }
+    static void reverseinsertion(int[]arr){            //ouput {5,4,3,2,1}
+        for(int i = 0; i<arr.length-1; i++){
+            for(int j = i+1; j>0; j--){
+                if(arr[j]>arr[j-1]){
+                    swap(arr, j, j-1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
     }
     static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
